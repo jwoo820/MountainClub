dcf373607d3eb33ec4a18c7368e9cfe1c018e160
jwoo820 <pjwoo820@gmail.com>
2022-06-17 15:32:04 +0900

    프로젝트 생성

---
+++ GoodPeople/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodPeople
+//
+//  Created by Park Jungwoo on 2022/06/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
